203226dea355c8dc94c096752459fa53ed7c221f
JEROLPOA2 <jledesmap@unal.edu.co>
2022-09-03 13:04:12 -0500

    Update Tipo.java

--- src/main/java/classroom/Tipo.java
+++ src/main/java/classroom/Tipo.java
@@ -3,11 +3,7 @@ package classroom;
 public enum Tipo {
 	DISCIPLINAR(10),FUNDAMENTACION(20),ELECTIVA(30);
 	
-	/*private int codigo;
-	private String nombre;
-	
-	Tipo(int codigo,String nombre) {
+	Tipo(int codigo) {
 		this.codigo = codigo;
-		this.nombre = nombre;
-	}/* */
+	}
 }
